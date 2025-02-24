ea8a05b510243d91b563953f1c901b4365858fc8
Deekshit Addepalli <deaddepa@microsoft.com>
2025-02-23 16:07:29 -0800

    Initial push with migration added

---
+++ UdemyDating.WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using UdemyDating.WebAPI.Models;
+
+namespace UdemyDating.WebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ UdemyDating.WebAPI/Models/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyDating.WebAPI.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        
+        public required string UserName { get; set; } //site user?
+
+        
+        public required string UserFirstName { get; set; }
+        
+        public required string UserLastName { get; set; }
+        
+
+    }
+}
