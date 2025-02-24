b63881d81058fdf5e1cb8fc3d26476b7dc3219e0
Deekshit Addepalli <deaddepa@microsoft.com>
2025-02-23 16:33:49 -0800

    UserController added

---
+++ UdemyDating.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using UdemyDating.WebAPI.Models;
+using System.Linq;
+using UdemyDating.WebAPI.Data;
+
+namespace UdemyDating.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext dbContext;
+        private readonly ILogger<UsersController> logger;
+
+        public UsersController(AppDbContext dbContext, ILogger<UsersController> logger) // Injecting our AppDbContext class here since it is already instantiated in the Program.cs file. The AppDbContext file is under the Data folder.
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            logger.LogInformation("Getting all users");
+            var users = dbContext.Users.ToList();
+
+            return Ok(users);
+
+        }
+        [HttpGet("{Id}")]
+        public ActionResult<AppUser> GetUser(int Id)
+        {
+            logger.LogInformation("Getting user " +Id);
+            var user = dbContext.Users.FirstOrDefault(x => x.Id == Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(user);
+            }
+
+        }
+
+    }
+}
